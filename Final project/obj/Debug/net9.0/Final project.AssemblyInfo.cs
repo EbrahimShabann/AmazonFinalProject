6f6e09e9a97f04c927c8c30d624e949578c1f6cc
EbrahimShabann <ibrahimshaban247@gmail.com>
2025-07-20 13:31:57 +0300

    init

--- Final project/obj/Debug/net9.0/Final project.AssemblyInfo.cs
+++ Final project/obj/Debug/net9.0/Final project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e87abfbf9f67f75a02912ecdc1236433761bfc21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c23039d4aa23e17c63240bf111da6519e58df604")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
