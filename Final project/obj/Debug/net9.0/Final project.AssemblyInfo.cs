a8832e2e72af2031ac507ba2b7a14d68e2c455f5
EbrahimShabann <ibrahimshaban247@gmail.com>
2025-07-29 21:14:13 +0300

    Resolve merge conflicts by removing bin/ and obj/

--- Final project/obj/Debug/net9.0/Final project.AssemblyInfo.cs
+++ Final project/obj/Debug/net9.0/Final project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a5f1430357aeb6a5b893c6f9849242aa151f5b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c5b5784b1fe448a42c91b3b5f383273d6ed8e1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
